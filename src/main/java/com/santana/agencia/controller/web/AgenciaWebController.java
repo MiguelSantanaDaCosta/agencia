3b799238a8a634ee1c611bd254229c4011da1f36
Avraham <souzaavraham@proton.me>
2025-06-05 01:24:05 -0300

    Início da agencia web

---
+++ src/main/java/com/santana/agencia/controller/web/AgenciaWebController.java
@@ -0,0 +1,26 @@
+package com.santana.agencia.controller.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.santana.agencia.service.ViagemService;
+
+
+/**
+ * AgenciaWebController
+ */
+@Controller
+@RequestMapping("/")
+public class AgenciaWebController {
+  @Autowired
+  private ViagemService viagemService;
+
+  @GetMapping
+  public String index(Model model) {
+    model.addAttribute("viagens", viagemService.listarTodos());
+    return "Agencia";
+  }
+}
